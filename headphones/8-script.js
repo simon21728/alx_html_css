ed0de9de28f111ab9ac6162ad93c2b6aab55733b
Simon <simonbayu2211@exgmail.com>
2025-12-09 10:54:34 +0300

    Humberger Menu. Mmmm

---
+++ headphones/8-script.js
@@ -0,0 +1,7 @@
+// Hamburger menu toggle
+const menuIcon = document.getElementById('menu-icon');
+const navMenu = document.getElementById('nav-menu');
+
+menuIcon.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
